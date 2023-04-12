98788c343459cedf6b599775fd36e1633d641900
Alissa <alissatroianonyc@gmail.com>
2023-04-11 21:10:11 -0400

    Add docstring

--- mindsdb/integrations/handlers/coinglass_handler/coinglass_handler.py
+++ mindsdb/integrations/handlers/coinglass_handler/coinglass_handler.py
@@ -6,22 +6,24 @@ from mindsdb.integrations.libs.response import HandlerStatusResponse
 
 
 class CoinglassHandler(APIHandler):
+    """
+    A class for handling connections and calls to the Coinglass API
+    """
     def __init__(self, name: str):
         super().__init__(name)
         """ constructor
         Args:
             name(str): the handler's name
         """
-
         self._tables = {}
 
 
-def register_table(self, table_name: str, table_class: Any):
-    """
-    Register coinglass data table(s) here (example, 'coins');
-    """
-    self._tables[table_name] = table_class
+    def register_table(self, table_name: str, table_class: Any):
+        """
+        Register coinglass data table(s) here (example, 'coins');
+        """
+        self._tables[table_name] = table_class
 
 
-def connect(self) -> HandlerStatusResponse:
-    """"""
+    def connect(self) -> HandlerStatusResponse:
+        """"""
