55c07aabe065f99cf69c2061e273f671c583a550
Alissa Troiano <alissatroianonyc@gmail.com>
2024-02-06 22:18:14 -0500

    Start  testing access token and connection to handler

---
+++ mindsdb/integrations/handlers/instagram_handler/README.md
@@ -0,0 +1,18 @@
+# Automating Instagram Posts
+
+## Setup
+
+Tools needed to setup handler:
+
+1. Python
+
+2. instagrapi Library: a Python wrapper for the Instagram Private API. This library allows you to interact with Instagram's features programmatically
+
+3. Instagram Account
+
+4. Instagram Graph API Credentials
+
+### Dependencies
+
+- instagrapi - ***a Python wrapper for the Instagram Private API. This library allows you to interact with Instagram's features programmatically.***
+301888371410982|2obcZ-uDOuNqJ_QvykCqNeafD_M
---
+++ mindsdb/integrations/handlers/instagram_handler/instagram_handler.py
@@ -0,0 +1,22 @@
+import requests
+import os
+import env
+
+fb_client_id = os.getenv('fb_client_id')
+print(fb_client_id)
+fb_client_secret = os.getenv('fb_client_secret')
+print(fb_client_secret)
+fb_access_token = os.getenv('fb_access_token')
+# print(fb_access_token)
+fb_access_url = os.getenv('fb_access_url')
+# print (fb_access_url)
+
+graph_url = 'https://graph.facebook.com/v19.0/'
+
+def get_comment(ig_media_id = '' , fb_access_token = ''):
+    url = graph_url + ig_media_id + '/comments'
+    param = dict()
+    param['fb_access_token'] = fb_access_token
+    response = requests.get(url,param)
+    response = response.json()
+    return response
