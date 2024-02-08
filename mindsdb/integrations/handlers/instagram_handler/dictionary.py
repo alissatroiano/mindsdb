e6e899ef3ac389e100f83ed94c4ed8068eb9c93a
Alissa Troiano <alissatroianonyc@gmail.com>
2024-02-08 18:30:00 -0500

    Add new logic to obtain long lived access token

--- mindsdb/integrations/handlers/instagram_handler/dictionary.py
+++ mindsdb/integrations/handlers/instagram_handler/dictionary.py
@@ -15,7 +15,7 @@ fb_access_url = os.getenv('fb_access_url')
 
 def getCreds():
     creds = dict()
-    creds['access_token'] = access_token
+    creds['access_token'] = os.getenv('fb_access_token')
     creds['client_id'] = client_id
     creds['client_secret'] = client_secret
     creds['graph_domain'] = 'https://graph.facebook.com/'
@@ -25,13 +25,13 @@ def getCreds():
 
     return creds
 
-def make_api_call( url, endpoint_base, debug = 'no' ) :
-    data = requests.get( url, endpoint_base )
+def make_api_call( url, endpoint_params, debug = 'no' ) :
+    data = requests.get( url, endpoint_params )
 
     response = dict()
     response['url'] = url
-    response['endpoint_base'] = endpoint_base
-    response['endpoint_base_pretty'] = json.dumps(endpoint_base, indent=4)
+    response['endpoint_params'] = endpoint_params
+    response['endpoint_params_pretty'] = json.dumps(endpoint_params, indent=4)
     response['json_data'] = json.loads(data.content)
     response['json_data_pretty'] = json.dumps( response ['json_data'], indent = 4 )
 
@@ -44,6 +44,6 @@ def display_api_call(response):
     print("\nURL: ")
     print(response['url'])
     print("\nEndpoint Params: ")
-    print(response['endpoint_base_pretty'])
+    print(response['endpoint_params_pretty'])
     print("\nResponse: ")
     print(response['json_data_pretty'])
--- mindsdb/integrations/handlers/instagram_handler/long_lived_access_token.py
+++ mindsdb/integrations/handlers/instagram_handler/long_lived_access_token.py
@@ -1,34 +1,4 @@
 from dictionary import getCreds, make_api_call
-import datetime
-import os
-import env
-
-client_id = os.getenv('fb_client_id')
-# print(fb_client_id)
-client_secret = os.getenv('fb_client_secret')
-# print(fb_client_secret)
-access_token = os.getenv('fb_access_token')
-# print(fb_access_token)
-access_url = os.getenv('fb_access_url')
-# print (fb_access_url)
-
-graph_url = 'https://graph.facebook.com/v19.0/me/accounts'
-
-def debug_access_token(params):
-    endpoint_params = dict()
-    endpoint_params['input_token'] = params['access_token']
-    endpoint_params['access_token'] = params['access_token']
-    url = params['graph_domain'] + '/debug_token'
-
-    return make_api_call( url, endpoint_params, params['debug'] )
-
-params = getCreds()
-params['debug'] = 'yes'
-response = debug_access_token(params)
-
-print("\nExpires at: ")
-print (datetime.datetime.fromtimestamp( response['json_data']['data']['data_access_expires_at'] ))
-
 
 def get_long_lived_access_token(params):
     endpoint_params = {
@@ -45,8 +15,10 @@ def get_long_lived_access_token(params):
 params = getCreds()
 params['debug'] = 'yes'
 access_token_key = 'access_token'  # Define the key to access the access token
+
 response = get_long_lived_access_token(params)
-print("\n ---- LONG LIVED ACCESS TOKEN: ----\n")
+
+print("\n ---- ACCESS TOKEN INFO ----\n")
 
 # Check if the 'json_data' key is present in the response
 if 'json_data' in response:
